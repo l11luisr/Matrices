e11bce729b7c9204232d58013e978b010cb98c27
José Luis Rodríguez Muñoz <47265230+l11luisr@users.noreply.github.com>
2019-10-31 23:52:36 -0700

    Update matriz.c

--- matriz.c
+++ matriz.c
@@ -1,17 +1,3 @@
-/* Programación Orientada a Objetos
-   Alumno: Rodríguez Muñoz José Luis
-   Profesor: Reyes Juárez
-   Grupo: 541
-
-   Descripción:
-   Programa en C con estructuras que consiste en operaciones con matrices entre ellas:
-   1. Imprime las matrices A y B
-   2. Imprime sus determinantes
-   3. Imprime la suma de la Matriz A y B
-   4. Imprime la resta de la Matriz A y B
-   5. Imprime la multiplicación de la Matriz A y B
-   6. Imprime la transpuesta */
-
 #include "matriz.h"
 #include "matriz.h"
 #include <math.h>
@@ -196,7 +182,7 @@ void printDatos(Matriz m) {
 		printf("\n");
 	}
 
-	/* Imprime la operación de Matriz A + Matriz B */
+	/* Imprime la operaciÃ³n de Matriz A + Matriz B */
 
 	printf("\n Matriz A + Matriz B:\n");
 	for(i = 0; i < 3; i++) {
@@ -206,7 +192,7 @@ void printDatos(Matriz m) {
         printf("\n");
 	}
 
-	/* Imprime la operación de Matriz A - Matriz B */
+	/* Imprime la operaciÃ³n de Matriz A - Matriz B */
 
 	printf("\n Matriz A - Matriz B:\n");
 	for(i = 0; i < 3; i++) {
@@ -216,7 +202,7 @@ void printDatos(Matriz m) {
         printf("\n");
 	}
 
-	/* Imprime la operación de Matriz A * Matriz B */
+	/* Imprime la operaciÃ³n de Matriz A * Matriz B */
 
 	printf("\n Matriz A * Matriz B:\n");
 	for(i = 0; i < 3; i++) {
